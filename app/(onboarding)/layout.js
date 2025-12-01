dc158cbc1b096f77468415a084d8c2888883e05a
Esseh12 <ese.gbenmo@salario.africa>
2025-12-02 00:42:44 +0100

    auth image update

--- app/(onboarding)/layout.js
+++ app/(onboarding)/layout.js
@@ -2,20 +2,30 @@
 
 export default function AuthLayout({ children }) {
 	return (
-		<div className='min-h-screen flex flex-col lg:flex-row max-h-screen'>
-			{/* Image Section - Right Side */}
-			<div className='hidden lg:block lg:w-1/2 relative bg-linear-to-br from-pink-100 to-purple-100 rounded-tr-xl rounded-br-xl'>
+		<div className='max-h-screen flex flex-col lg:flex-row items-stretch p-1.5'>
+			{/* LEFT — IMAGE + TEXT */}
+			<div className='hidden lg:flex relative w-[640px] items-end'>
 				<img
-					src='/copy-space-cleaning-cosmetic-product.jpg'
+					src='/auth-img.svg'
 					alt='auth'
-					className='w-full h-full rounded-tr-2xl rounded-br-2xl'
-					height={0}
-					width={0}
+					className='w-full h-full max-h-screen object-cover rounded-xl'
 				/>
+
+				{/* TEXT AT BOTTOM OF IMAGE */}
+				<div className='absolute bottom-6 left-6 right-6 text-white space-y-2 max-w-[645px]'>
+					<h1 className='font-bold text-4xl leading-13.5'>
+						Expert skincare tips designed for every skin type
+					</h1>
+					<p>
+						Learn how to care for your skin the right way, with tips that makes
+						your routine easier and more effective.
+					</p>
+				</div>
 			</div>
-			{/* Form Section - Left Side */}
-			<div className='w-full lg:w-1/2 flex items-center justify-center p-6 sm:p-8 lg:p-12'>
-				<div className='w-full max-w-md'>{children}</div>
+
+			{/* RIGHT — FORM AREA */}
+			<div className='flex flex-1 items-center justify-center p-6 sm:p-8 lg:p-12'>
+				<div className='w-full max-w-md lg:max-w-lg'>{children}</div>
 			</div>
 		</div>
 	);
